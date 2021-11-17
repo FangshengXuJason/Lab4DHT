a605016ddb4b537a2852ee7f47a40b33c5c07d43
FangshengXuaJason <xufangsheng@seattleu.edu>
2021-11-16 22:49:16 -0800

    Initial commit

---
+++ ChordNode.py
@@ -0,0 +1,5 @@
+
+
+BUF_SZ = 4096  # socket recv arg
+BACKLOG = 100  # socket listen arg
+TEST_BASE = 43544  # for testing use port numbers on localhost at TEST_BASE+n
---
+++ ModRange.py
@@ -0,0 +1,67 @@
+
+class ModRange(object):
+    """
+    Range-like object that wraps around 0 at some divisor using modulo arithmetic.
+
+    >>> mr = ModRange(1, 4, 100)
+    >>> mr
+
+    >>> 1 in mr and 2 in mr and 4 not in mr
+    True
+    >>> [i for i in mr]
+    [1, 2, 3]
+    >>> mr = ModRange(97, 2, 100)
+    >>> 0 in mr and 99 in mr and 2 not in mr and 97 in mr
+    True
+    >>> [i for i in mr]
+    [97, 98, 99, 0, 1]
+    >>> [i for i in ModRange(0, 0, 5)]
+    [0, 1, 2, 3, 4]
+    """
+
+    def __init__(self, start, stop, divisor):
+        self.divisor = divisor
+        self.start = start % self.divisor
+        self.stop = stop % self.divisor
+        # we want to use ranges to make things speedy, but if it wraps around the 0 node, we have to use two
+        if self.start < self.stop:
+            self.intervals = (range(self.start, self.stop),)
+        elif self.stop == 0:
+            self.intervals = (range(self.start, self.divisor),)
+        else:
+            self.intervals = range(self.start, self.divisor), range(self.stop)
+
+    def __repr__(self):
+        """ Something like the interval|node charts in the paper """
+        return ''.format(self.start, self.stop, self.divisor)
+
+    def __contains__(self, id) -> bool:
+        """ Is the given id within this finger's interval? """
+        return any(id in interval for interval in self.intervals)
+
+    def __len__(self):
+        return sum(len(interval) for interval in self.intervals)
+
+    def __iter__(self):
+        return ModRangeIter(self, 0, -1)
+
+
+class ModRangeIter(object):
+    """ Iterator class for ModRange """
+    def __init__(self, mr, i, j):
+        self.mr, self.i, self.j = mr, i, j
+
+    def __iter__(self):
+        return ModRangeIter(self.mr, self.i, self.j)
+
+    def __next__(self):
+        if self.j == len(self.mr.intervals[self.i]) - 1:
+            if self.i == len(self.mr.intervals) - 1:
+                raise StopIteration()
+            self.i += 1
+            self.j = 0
+        else:
+            self.j += 1
+        return self.mr.intervals[self.i][self.j]
+
+
